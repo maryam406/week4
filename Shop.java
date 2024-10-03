b71cf418d0a5cf3e6653ed5286049bb2063b3fb0
maryam406 <sp24-bse-058@cuilahore.edu.pk>
2024-10-03 06:09:16 +0500

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -1,61 +1,97 @@
 import java.util.Scanner;
+
 public class Shop{
-
-       private String Shopname;
-       private String owner;
-       private Product list[]=new Product[100];
-     private static int counter=0;
-     // constructor
-       public Shop(String Shopname,String owner,Product product){
-          this.Shopname=Shopname;
-          this.owner=owner;
-          this.product=product;}
-      //
-        
-       public void setShopname(String S){
-              this.Shopname=S; }
-
-       public String getShopename(){
-          return Shopname;}
-      
-        public void setowner(String o){
-              this.owner=o; }
-
-       public String getowner(){
-          return owner;}
-
-        // function to add
-
-       public void addproduct(){
-  
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter Product details");
-        System.out.print("Name: ");
-        String name = input.nextLine();
-        System.out.print("Price: ");
-        double price = input.nextDouble();
-        System.out.print("Quantity: ");
-        int quantity = input.nextInt();
-        Product product =new Product(name,prize,quatity);
-        Productlist[productcount++]=product;
-        System.out.println("Product added");
-}
-
-       // funtion to sell
-
-       public void sale(){
-         System.out.println("Sale Menu");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter product ID: ");
-        String id= scanner.nextLine();
- 
-        for(int i=0; i<productCount ;i++){
-              if(productlist[i].getId.equals(id)){
-                  productList[i].setQuantity(productList[i].getQuantity()-1);
-                  revenue+=productList[i].getPrice();
-                  System.out.println("Sale successful");}
-            else{
-                System.out.println("Product not found");
-           }}
-}
+   private Product pro_list[]=new Product[20];
+   private Person owner;
+   private String address;
+   private static double revenue;
+   private static int pro_count=0;
+   
+   public Shop(Person owner,String address){
+	   this.owner=owner;
+	   this.pro_list=new Product[20];
+	   this.address=address;
+   }
+   
+   public void add_pro(){
+	   Scanner input=new Scanner(System.in);
+	   System.out.println("Adding details of Product : ");
+	   System.out.println("Name :");
+	   String name=input.nextLine();
+	   System.out.println("Price :");
+	   double price=input.nextDouble();
+	   System.out.println("Quantity : ");
+	   int quantity=input.nextInt();
+	   Product product=new Product(name,price,quantity);
+	   pro_list[pro_count]=product;
+	   System.out.println("Successfully Added");
+	   pro_count++; 
+	   
+   }
+   public void sale(){
+	   System.out.println("Sale --->>>>");
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter Product Id : ");
+	   String id=sc.nextLine();
+	   for(int i=0;i<pro_count;i++){
+		   if (pro_list[i].getId().equals(id))
+		   {
+			   pro_list[i].setQuantity(pro_list[i].getQuantity()-1);
+			   revenue+=pro_list[i].getPrice();
+			   System.out.println("Sale Successfully");
+		   }
+	   }
+   }
+   
+   public void modify(){
+	   System.out.println("Modifying the product");
+	   Scanner sc2=new Scanner(System.in);
+	   System.out.println("Enter Product Id which u want to modify: ");
+	   String id=sc2.nextLine();
+	   System.out.println("Now tell me the details :");
+	   System.out.println("Name ; ?");
+	   String name=sc2.nextLine();
+	   System.out.println("Price : ?");
+	   double price=sc2.nextDouble();
+	   System.out.println("Quantity : ?");
+	   int quantity=sc2.nextInt();
+	   for(int i=0;i<pro_count;i++){
+		   if (pro_list[i].getId().equals(id))
+		   {
+			   pro_list[i].setName(name);
+			   pro_list[i].setPrice(price);
+			   pro_list[i].setQuantity(quantity);
+			   System.out.println("Successfully Modified");
+			   break;
+		   }
+	   
+	   }
+   }
+   
+   public void remove_pro() {
+    System.out.println("Removing a product");
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter Product Id of the product you want to remove: ");
+    String id = sc.nextLine();
+    for (int i = 0; i < pro_count; i++) {
+        if (pro_list[i].getId().equals(id)) {
+            
+            pro_list[i] = null; // Simply set the product to null
+            System.out.println("Product removed successfully.");
+            break;
+        }
+    }
+   }
+   
+   //display method 
+   public void display() {
+        System.out.println("Displaying Products:");
+		System.out.println("Name : "+owner.getName()+"   Email : "+owner.getEmail()+"Address : "+address);
+		
+        for (int i = 0; i < pro_count; i++) {
+            if (pro_list[i] != null) {
+                System.out.println(pro_list[i]); 
+            }
+        }
+   }
 }
